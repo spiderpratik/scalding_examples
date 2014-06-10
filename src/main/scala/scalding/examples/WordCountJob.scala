02cb71778144bfe1ea1d7cf11c02b247ae9eb5c6
pratikpp <spiderpratik@rediffmail.com>
2014-06-10 11:49:46 +0530

    commit

--- src/main/scala/scalding/examples/WordCountJob.scala
+++ src/main/scala/scalding/examples/WordCountJob.scala
@@ -1,14 +1,23 @@
 package scalding.examples
 
 import com.twitter.scalding._
+//import scala.util.matching.Regex
 
 class WordCountJob(args : Args) extends Job(args) {
-  TextLine( args("input") )
+  val extractor = """(http|https)://([^.]+[w|1]\.|)([^.]+\.[^/]+)/|.+""".r
+
+  /*TextLine( args("input") )
     .flatMap('line -> 'word) {
       line : String => tokenize(line)
     }
     .groupBy('word) { _.size }
-    .write( Tsv( args("output") ) )
+    .write( Tsv( args("output") ) )*/
+
+  TextLine( args("input") )
+    .map('line -> 'word) {line : String => {extractor.findAllIn(line).matchData.toList(0).group(3)}}
+    .groupBy('word) { gp: GroupBuilder => gp.size }
+    .filter('size){size: Int => size > 4}
+    .write(Tsv(args("output")))
 
   // split a piece of text into individual words
   def tokenize(text : String) : Array[String] = {
@@ -20,8 +29,8 @@ object WordCountJob extends App {
   val progargs: Array[String] = List(
     "-Dmapred.map.tasks=200",
     "scalding.examples.WordCountJob",
-    "--input", "/tmp/files",
-    "--output", "/tmp/wordcount.txt",
+    "--input", "/home/india/Desktop/Indix/scalding_examples/src/main/resources/sampleurls.txt",
+    "--output", "/home/india/Desktop/Indix/scalding_examples/src/main/resources/output",
     "--hdfs"
   ).toArray
   Tool.main(progargs)
