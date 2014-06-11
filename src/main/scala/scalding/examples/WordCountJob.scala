12d1b12e2ba878f18451918d88c7279e636452e4
pratikpp <spiderpratik@rediffmail.com>
2014-06-11 10:46:11 +0530

    using java.net.URL

--- src/main/scala/scalding/examples/WordCountJob.scala
+++ src/main/scala/scalding/examples/WordCountJob.scala
@@ -1,10 +1,9 @@
 package scalding.examples
 
 import com.twitter.scalding._
-//import scala.util.matching.Regex
 
 class WordCountJob(args : Args) extends Job(args) {
-  val extractor = """(http|https)://([^.]+[w|1]\.|)([^.]+\.[^/]+)/|.+""".r
+  //val extractor = """(http|https)://([^.]+[w|1]\.|)([^.]+\.[^/]+)/|.+""".r
 
   /*TextLine( args("input") )
     .flatMap('line -> 'word) {
@@ -14,15 +13,22 @@ class WordCountJob(args : Args) extends Job(args) {
     .write( Tsv( args("output") ) )*/
 
   TextLine( args("input") )
-    .map('line -> 'word) {line : String => {extractor.findAllIn(line).matchData.toList(0).group(3)}}
+    .map('line -> 'word) {line : String => {
+    /*extractor.findAllIn(line).matchData.toList(0).group(3)}*/
+    val host = new java.net.URL(line).getHost
+    if (host.startsWith("www"))
+      host.substring(host.indexOf('.')+1)
+    else host
+    }
+  }
     .groupBy('word) { gp: GroupBuilder => gp.size }
-    .filter('size){size: Int => size > 4}
+    .filter('size){size: Int => size >= 5}
     .write(Tsv(args("output")))
 
   // split a piece of text into individual words
-  def tokenize(text : String) : Array[String] = {
+  /*def tokenize(text : String) : Array[String] = {
     text.toLowerCase.split(" ")
-  }
+  }*/
 }
 
 object WordCountJob extends App {
